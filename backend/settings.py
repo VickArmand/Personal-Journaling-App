55a7e31709835d55aa81f56105dcec073297006d
VickArmand <victormaina1962@gmail.com>
2024-07-05 17:20:27 +0300

    _framework as an app

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend.categories',
-    'backend.journals'
+    'backend.journals',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
