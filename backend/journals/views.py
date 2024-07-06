83e01009145c49f745305e8db0e8bc7fceeb1e54
VickArmand <victormaina1962@gmail.com>
2024-07-07 00:37:17 +0300

    add summary views

--- backend/journals/views.py
+++ backend/journals/views.py
@@ -3,9 +3,11 @@ from django.http import JsonResponse
 from .models import Journal
 from .serializers import JournalSerializer
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def index(request):
     """"""
     journals = Journal.objects.all()
@@ -13,6 +15,7 @@ def index(request):
     return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def create(request):
     """"""
     serializer = JournalSerializer(data=request.data)
@@ -22,6 +25,7 @@ def create(request):
     return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['PUT'])
+@permission_classes([IsAuthenticated])
 def edit(request, id):
     """"""
     cid = id
@@ -36,6 +40,7 @@ def edit(request, id):
     return JsonResponse(serializer.data, status=status.HTTP_202_ACCEPTED)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def find(request, id):
     """"""
     cid = id
@@ -46,7 +51,29 @@ def find(request, id):
     serializer = JournalSerializer(journal)
     return JsonResponse(serializer.data, status=status.HTTP_200_OK)
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def summary(request):
+    """"""
+    from datetime import datetime
+    current_time = datetime.now()
+    journals_year = Journal.objects.filter(created_at__year = current_time.year)
+    journals_month = journals_year.filter(created_at__month=current_time.month)
+    journals_day = journals_year.filter(created_at__day=current_time.day)
+    week_start = current_time.day - current_time.weekday()
+    week_end = week_start + 6
+    journals_week = journals_year.filter(created_at__day__gt=week_start).filter(created_at__day__lt=week_end)
+
+    summary = {
+        "year": journals_year.count(),
+        "month": journals_month.count(),
+        "week": journals_week.count(),
+        "day": journals_day.count(),
+    }
+    return JsonResponse(summary, status=status.HTTP_200_OK, safe=False)
+
 @api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
 def delete(request, id):
     """"""
     cid = id
