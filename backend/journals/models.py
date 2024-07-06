51337c1f5b465e83dd157e8ff4c256e1442c18ae
VickArmand <victormaina1962@gmail.com>
2024-07-07 00:37:00 +0300

    add summary views

--- backend/journals/models.py
+++ backend/journals/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from backend.auth_users.models import CustomUser
 from backend.categories.models import Category
 
 class Journal(models.Model):
@@ -7,6 +7,6 @@ class Journal(models.Model):
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=2000)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
