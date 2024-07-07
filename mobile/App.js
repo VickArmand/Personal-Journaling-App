ac11311858473beca05136cb821daea6c11cd52a
VickArmand <victormaina1962@gmail.com>
2024-07-07 17:58:50 +0300

    Setup login

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,47 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+const uri = "http://localhost:8000/"
 export default function App() {
+  state = {}
+  console.log(state)
+  function login(request) {
+    const response = axios.post(`${uri}users/login`, request).then((result) => {
+      if (result.data.success)
+        alert("Logged in")
+      else
+        alert(result.error)
+    }).catch((err) => alert(err.message))
+    return response
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style= {styles.header}>PERSONAL JOURNALING APPLICATION</Text>
       <StatusBar style="auto" />
+      <TextInput placeholder="Enter your Email Address" style={styles.input} onChangeText={(text) => state.email = text}/>
+      <TextInput placeholder="Enter your Password" style={styles.input} secureTextEntry password onChangeText={(text) => state.password = text}/>
+      <Button title="Log In" color="orange" onPress={() => login(state)}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 40,
+    padding: 10,
     flex: 1,
+    rowGap: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 3,
+    height: 40,
+    borderColor: 'blue',
+  }
 });
