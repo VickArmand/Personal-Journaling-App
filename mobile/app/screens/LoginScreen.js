cbd73d45bc682330b8198c02c5809ae7f94702b5
VickArmand <victormaina1962@gmail.com>
2024-07-24 04:39:46 +0300

    add logout

--- mobile/app/screens/LoginScreen.js
+++ mobile/app/screens/LoginScreen.js
@@ -6,10 +6,16 @@ import styles from '../styles/styles';
 
 state = {}
 async function login(request) {
-  await axios.post(`${uri}users/login`, request).then((res) => {
+  await axios.post(`${uri}/users/login`, request).then((res) => {
     console.log(res)
     alert("Login success");
   }).catch((err) => alert(err.response.data.error));
+}
+async function logout(keys) {
+  await axios.delete(`${uri}/users/logout`, {headers: {Cookie: `sessionid=${keys.session}; csrftoken=${keys.token};`, 'x-csrftoken': keys.token}}, {withCredentials: true}).then((res) => {
+    console.log(res)
+    alert(res.data.success);
+  }).catch((err) => console.log(err));
 }
 export default function LoginScreen() {
   return (
@@ -20,6 +26,7 @@ export default function LoginScreen() {
       <TextInput placeholder="Enter your Password" style={styles.input} secureTextEntry password onChangeText={(text) => state.password = text}/>
       <Button title="Log In" style={{color: 'orange'}} onPress={async () => await login(state)}/>
         Click here to Register
+      <Button title="Log Out" style={{color: 'orange'}} onPress={async () => await logout({session: "44334vla1q0u2rxain9btlvciybb11aa", token: "ixSxZBlC88LDtrp8p19IOhKZk6WhNiYhBtJg8HBZ17KcVFcxgdF0BIyFkMvI4P0d"})}/>
       </View>
     </View>
   );
