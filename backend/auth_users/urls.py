40a83cc678e1a999e2d184f30e8e74f801215a6f
VickArmand <victormaina1962@gmail.com>
2024-08-03 19:39:02 +0300

    change authentication views

--- backend/auth_users/urls.py
+++ backend/auth_users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenBlacklistView)
 
 urlpatterns = [
     path('register', views.register),
-    path('login', views.signin),
+    path('login', TokenObtainPairView.as_view()),
+    path('token/refresh', TokenRefreshView.as_view()),
     path('edit', views.edit_user),
-    path('logout', views.signout),
+    path('logout', TokenBlacklistView.as_view()),
 ]
