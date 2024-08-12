70793892db02528975972b2c60082308ae751620
VickArmand <victormaina1962@gmail.com>
2024-08-12 14:17:15 +0300

    Enable user to login using email than using username

--- backend/auth_users/models.py
+++ backend/auth_users/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser, PermissionsMixin):
-    username = None
+class CustomUser(AbstractUser):
     email = models.EmailField(max_length=200, unique=True, null=False)
-    password = models.CharField(max_length=500, null=False)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name','last_name','username', 'password']
